042a703066bcdeca06fcc32f7de69b68e6e947b8
AlmeidaNunesGabriel <dev.almeidanunesgabriel@gmail.com>
2025-03-30 21:37:02 -0300

    pequenas modificações

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import 'react-native-gesture-handler';
 import { Text, SafeAreaView, StyleSheet } from 'react-native';
-import { Card } from 'react-native-paper';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native'; 
-import AssetExample from './components/AssetExample';
 import Home from "./src/screens/Home";
-import { View} from 'react-native';
 import About from './src/screens/About';
 import Project from './src/screens/Project';
 
